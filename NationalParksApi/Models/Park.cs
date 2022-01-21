dd8cbf8384c8e08d1eedc18fb1e14bd5fb2fd76b
Katie Pundt <kzpundt@gmail.com>
2022-01-21 09:24:58 -0800

    add park properties

--- NationalParksApi/Models/Park.cs
+++ NationalParksApi/Models/Park.cs
@@ -1,7 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NationalParksApi.Models
 {
   public class Park
   {
-    
+    public int ParkId { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    [Required]
+    [StringLength(2)]
+    public string State { get; set; }
+
+    public string Description { get; set; }
+
+    [Required]
+    public bool Visited { get; set; }
+
   }
 }
